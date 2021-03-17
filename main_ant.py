57445d8a964772943093f0af31878b0760c12cc0
JNStrauss <80687022+JNStrauss@users.noreply.github.com>
2021-03-17 12:38:45 +0100

    Update main_ant.py

--- main_ant.py
+++ main_ant.py
@@ -3,8 +3,9 @@ import pygame
 from fourmi import Fourmi
 from ville import Town
 
+
 def update_screen(itinerary):
-    """given an itinerary this pocedure draws a possible solution
+    """given an itinerary this pocedure draws a possible solution starting from the first town (all_towns[0]), can be upgraded to analyse better the data it is feeded
 
     Args:
         itinerary (list): the towns in the order the computer should go through
@@ -16,6 +17,7 @@ def update_screen(itinerary):
     pygame.draw.line(screen, (100,100,100), (itinerary[-1].x , itinerary[-1].y ), (itinerary[0].x , itinerary[0].y ), width = 2)
     # print("done")
 
+    
 def next_screen():
     """
     determines a not to bad itinerary
@@ -29,11 +31,9 @@ def next_screen():
             i += 1
         itinerary.append(future[i])
     update_screen(itinerary)
+
     
-
 pygame.init()
-
-
 
 SCREEN_HEIGHT = 700
 SCREEN_WIDTH = 1 * SCREEN_HEIGHT
@@ -51,13 +51,13 @@ for town in range(TOWN_NUMBER):
     all_towns.append(Town(town, TOWN_NUMBER, RADIUS, SCREEN_HEIGHT, SCREEN_WIDTH, []))
 
 for town in all_towns:
+    # we update the all_towns attribute, so that every town can have access to all of the towns
     town.all_towns = all_towns
 
 # we create the ants
 for ant in range(ANT_NUMBER):
     all_ants.append(Fourmi(ant, all_towns[randint(0, TOWN_NUMBER - 1)], TOWN_NUMBER, SPEED))
     
-
 
 screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
 
@@ -66,7 +66,7 @@ background = pygame.image.load('carte0.jpg')
 launched = True
 
 while launched:
-    
+    # show the background
     screen.blit(background, (-200, -200))
 
     # we update the screen
@@ -76,13 +76,15 @@ while launched:
     # we move the ants
     for fourmi in all_ants:
         fourmi.move()
-        
+       
+    # total_pheromone is a counter to determine when the program is to stop by itself
     total_pheromone += 10
     
     if total_pheromone > 1000000:
         launched = False
-    for event in pygame.event.get():
         
+    for event in pygame.event.get():
+        # if the event if closing the window
         if event.type == pygame.QUIT:
             launched = False
             pygame.quit()
